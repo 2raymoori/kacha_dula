e4ad490e6b0c66db2a166b212361d7eaf9ab56a9
2raymoori <lo2raymoori@gmail.com>
2022-12-08 22:20:21 +0100

    Comit for profile Module

--- src/Controller/Profile.Controller.js
+++ src/Controller/Profile.Controller.js
@@ -4,18 +4,41 @@ const profileSchema = require('../Model/Profile.Model');
 const addProfile=async(req,res)=>{
     try {
         console.log('#############################################################');
-        
-        if(req.files){
-            const img  = req.files.pImage;
-            const fExtension = img.name.split(".")[img.name.split(".").length-1];
-            //img.mv(`./ProfileImgs/lot.${fExtension}`);
-        }
-        const {gender,nationality,pno} = req.body;
-        const searchUserProfile = profileSchema.find();
+       
+        const searchUserProfile = await profileSchema.find();
         console.log(searchUserProfile);
-        if(searchUserProfile){
+        if(searchUserProfile.length >0){   
+            return res.status(201).json({
+            status: "Error",
+            msg: {
+              info: "Sorry There exists an error. Already A profile exists in the System associated to this user.",
+            },
+          });
             console.log("There exists a user in the system with this profile.");
-        }else{
+        }else{ 
+            const {gender,nationality,pno} = req.body;
+            const newProfile = new profileSchema();
+            if(req.files){
+                const img  = req.files.pImage;
+                const fExtension = img.name.split(".")[img.name.split(".").length-1];
+                const imgName = `${req.user.id}.${fExtension}`;
+                img.mv(`./ProfileImgs/${imgName}`);
+                newProfile.img = imgName;
+            }
+            newProfile.pno = pno;
+            newProfile.user = req.user.id;
+            newProfile.gender = gender;
+            newProfile.nationality = nationality;
+            newProfile.status = 1;
+            newProfile.friends = [];
+
+            await newProfile.save();
+            return res.status(200).json({
+              status: "Success",
+              msg: { info: "Profile Successfully Saved", data: newProfile },
+            });
+
+
             console.log("No profile associated with this id. Go ahead and add...");
         }
         console.log('#############################################################');
@@ -32,6 +55,21 @@ const addProfile=async(req,res)=>{
 const profile=async(req,res)=>{
     try {
         
+    const curId = req.params.id;
+    const findProfile = await profileSchema.findById(curId);
+    if (findProfile === null) {
+      return res.status(201).json({
+        status: "Error",
+        msg: { info: "Sorry No Such profile in the System with this Id" },
+      });
+    }
+    
+    return res.status(200).json({
+      status: "Success",
+      msg: { data: findProfile },
+    });
+
+        
     } catch (error) {
         return res.status(500).json({
           status: "Error",
@@ -43,28 +81,46 @@ const profile=async(req,res)=>{
 }
 const allProfile=async(req,res)=>{
     try {
-        console.log(req.user);
-        console.log("Fetching all Profile for What ever reason...")
-    } catch (error) {
+        const profiles = await profileSchema.find(
+          {},
+          {
+            password: 0,
+          }
+        );
+        // console.log(users);
+        return res.status(200).json({
+          status: "Success",
+          msg: { data: profiles },
+        });
+      } catch (error) {
         return res.status(500).json({
           status: "Error",
-          msg: { info: "Sorry There exittss an error in the server" },
+          msg: { info: "Sorry There exits an error in the server" },
         });
-        
-    }
+      }
 
 }
 const deleteProfile=async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        return res.status(500).json({
-          status: "Error",
-          msg: { info: "Sorry There exittss an error in the server" },
-        });
-        
+  try {
+    const curId = req.params.id;
+    const findProfile = await profileSchema.findByIdAndDelete(curId);
+    if (findProfile === null) {
+      return res.status(201).json({
+        status: "Error",
+        msg: { info: "Sorry No Such profile in the System with this Id" },
+      });
     }
-
+    return res.status(200).json({
+      status: "Success",
+      msg: { data: findProfile },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "Error",
+      msg: { info: "Sorry There exittss an error in the server" },
+    });
+  }
 }
 const modifyProfile=async(req,res)=>{
     try {
--- src/Router/Profile.Route.js
+++ src/Router/Profile.Route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { allProfile, profile, addProfile, modifyProfile, deleteProfile } = require('../Controller/Profile.Controller');
 const authenticate = require('../MiddleWare/Auth');
 const Router = express.Router();
-Router.get('/',authenticate,allProfile);
+Router.get('/',allProfile);
 Router.get('/:id',profile);
 Router.post('/',[authenticate],addProfile);//[authenticate]
 Router.put('/:id',modifyProfile);
